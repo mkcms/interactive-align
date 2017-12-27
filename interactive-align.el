7412c933d8981d9c7b3bd110a8fe25fd5b8a9405
Michał K <k.michal@zoho.com>
2017-12-27 17:15:56 +0100

    Add license in interactive-align.el

--- interactive-align.el
+++ interactive-align.el
@@ -7,6 +7,21 @@
 ;; Version: 0.0.1
 ;; Keywords: tools, editing, align, interactive
 
+;; Copyright (C) 2017 Michał Kondraciuk
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; This package provides command `ia-interactive-align'
